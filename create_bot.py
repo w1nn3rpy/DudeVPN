9ebcb391ce68c94a16105cb6b73dee713a25b6d4
w1nn3rpy <l33sa1337@gmail.com>
2024-09-26 21:27:03 +0400

    edit logs format

--- create_bot.py
+++ create_bot.py
@@ -6,8 +6,11 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from decouple import config
 
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(levelname)s %(filename)d: '
+           '[%(asctime)s] - %(message)s',
+    datefmt='%d-%m-%Y %H:%M:%S')
 logger = logging.getLogger(__name__)
 
 bot = Bot(token=config('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))

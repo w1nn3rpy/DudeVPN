92752b6560ce256c326964b6187bfa4adceab79e
w1nn3rpy <l33sa1337@gmail.com>
2025-01-12 07:11:13 +0400

    edit logger

--- create_bot.py
+++ create_bot.py
@@ -8,19 +8,14 @@ from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from decouple import config
 
-
-file_handler = TimedRotatingFileHandler(
-    filename='logs.log', when='midnight', interval=1, backupCount=7, encoding="utf-8"
-)
-console_handler = logging.StreamHandler(sys.stdout)
-
 logging.basicConfig(
     level=logging.INFO,
     format='%(levelname)s - %(filename)s:%(lineno)d: [%(asctime)s] - %(message)s',
     datefmt='%d-%m-%Y %H:%M:%S',
-    handlers=[file_handler, console_handler]
+    handlers=[logging.StreamHandler(), logging.handlers.TimedRotatingFileHandler('logs.log', when='midnight', interval=1, backupCount=7)]
 )
 logger = logging.getLogger(__name__)
 
 bot = Bot(token=config('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher(storage=MemoryStorage())
+

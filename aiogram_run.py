5457ea965ef0f12cb9720e1e243d1e29fc0d8e19
Dmitry <l33sa1337@gmail.com>
2026-01-04 17:59:23 +0400

    trytofix

--- aiogram_run.py
+++ aiogram_run.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from create_bot import bot, dp, logger
+from create_bot import create_bot, logger
 from handlers.admin_handlers import admin_router
 from handlers.crypto_payment_handlers import crypto_payment_router
 from handlers.start import start_router, set_commands
@@ -12,6 +12,7 @@ from handlers.common_payment_handlers import payment_router
 from handlers.ruble_payment_handlers import ruble_payment_router
 from handlers.stars_payment_handlers import stars_payment_router
 
+
 def start_scheduler():
     scheduler = AsyncIOScheduler()
     scheduler.add_job(check_end_subscribe, CronTrigger(hour=10), misfire_grace_time=180)
@@ -20,21 +21,32 @@ def start_scheduler():
 
 async def main():
     await create_table_if_not_exist()
-    # await check_end_subscribe()
-    start_scheduler()
+
+    bot, dp, client = await create_bot()
+
+    # register routers AFTER dispatcher exists
     dp.include_router(admin_router)
     dp.include_router(start_router)
     dp.include_router(payment_router)
     dp.include_router(ruble_payment_router)
     dp.include_router(stars_payment_router)
     dp.include_router(crypto_payment_router)
+
+    start_scheduler()
+
     await bot.delete_webhook(drop_pending_updates=True)
     await set_commands()
-    await dp.start_polling(bot)
 
-try:
-    if __name__ == '__main__':
+    try:
+        await dp.start_polling(bot)
+
+    finally:
+        await bot.session.close()
+        await client.close()
+
+
+if __name__ == "__main__":
+    try:
         asyncio.run(main())
-
-except KeyboardInterrupt as e:
-    logger.info('Shutdown requested')
+    except KeyboardInterrupt:
+        logger.info("Shutdown requested")
